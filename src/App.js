505c8a3cd8930ff432588e2626b675e7b3ece41f
jadashi97 <njadauniversal23@yahoo.com>
2023-02-27 22:47:44 -0600

    add more pages to mama's restaurant

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Home from './components/Home';
+import About from './components/About';
+import Menu from './components/Menu';
+
 
 function App() {
   return (
     <div>
       <Header/>
+      <Home/>
+      <About/>
+      <Menu/>
       <Footer/>
     </div>
   );
---
+++ src/components/Menu.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Menu = () => {
+  return (
+    <div>
+      <p>This is the Menu page</p>
+    </div>
+  )
+}
+
+export default Menu;

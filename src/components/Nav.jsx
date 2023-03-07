a62944c7b937115554e8127e20b22b5691b1f9e4
jadashi97 <njadauniversal23@yahoo.com>
2023-03-06 22:25:24 -0600

    add nav and API on app

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,10 +7,10 @@ const Nav = () => {
         <li><a href="/home">Home</a></li>
         <li><a href="/about">About</a></li>
         <li><a href="/menu">Menu</a></li>
-        <li><a href="/menu"></a></li>
+        <li><a href="/menu">Reserve Table</a></li>
       </ul>
     </div>
   )
 }
 
-export default Nav
+export default Nav;
